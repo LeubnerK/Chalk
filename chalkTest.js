f09862828c7292a11ea7e7aa8750cdd753a317bc
Kimberly <leubnerk@otc.edu>
2018-10-29 12:10:14 -0500

    Chalk js file

---
+++ chalkTest.js
@@ -0,0 +1,17 @@
+const chalk = require('chalk');
+ 
+console.log('Once upon a time, there was a cat named ' + chalk.red('Red') + '. He was an ' + chalk.redBright('orange') + 
+            ' tabby cat who loved to antagonize the dog, ' + chalk.blue('Blue') + '.');
+
+console.log('One day, ' + chalk.red('Red') + ' decided he would play a new prank on ' + chalk.blue('Blue') + ' - he would hide his dog bone inside of his litter box! ' + 
+            'This was going to be his greatest ' + chalk.green('(and stinkiest)') + ' prank yet!');
+
+console.log('As ' + chalk.blue('Blue') + ' was distracted by watching his ' + chalk.magenta('human') + ' play with a ball, ' + chalk.red('Red') + ' struck!');
+
+console.log('The big blob of a cat ran over to the bone and whisked it away, ' + chalk.yellow.underline('skampering as fast as he could!') + ' As he dropped the bone into his litterbox, ' + chalk.red('Red') + ' laughed with glee - this was going to be great!');
+
+console.log('But what ' + chalk.red('Red') + ' failed to see was ' + chalk.blue('Blue') + ' sneaking up behind him! For you see, ' + chalk.blue('Blue') + ' had a very strong nose and could smell ' + chalk.red('Red') + ' from a mile away!');
+
+console.log('As ' + chalk.red('Red') + ' turned away from his dirtywork, he met eye to eye with Blue and froze... ' + chalk.black.bgCyan('just in time to recieve a hundred drool-ridden kisses!'));
+
+console.log('From that day on, ' + chalk.red('Red') + ' thought twice about pranking ' + chalk.blue('Blue') + ' ... at least not until he had a fresh ' + chalk.cyan('bath') + ', thatcl is!');
